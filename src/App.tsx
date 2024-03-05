651b5864f8b07eb80e63f5694da16fd2535fcfa9
elizatuinei <55069280+elizatuinei@users.noreply.github.com>
2024-03-04 22:25:06 -0700

    setUpDefault

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // nothing is enforced by default so we have
+  // to install ESLint to make consis
   return (
     <div className="App">
       <header className="App-header">
