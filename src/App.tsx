2fdc96a840816ac3bb6682506666010acf29f347
elizatuinei <55069280+elizatuinei@users.noreply.github.com>
2024-03-07 10:53:38 -0700

    finalchanges

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CollegeBasketballTeams from './CollegeBasketballTeams.json';
+
+// const CollegeBasketballTeams = require('./CollegeBasketballTeams.json')
+interface Team {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+// these hav eto be contained in something
+function Welcome() {
+  return (
+    <div>
+      <p>March Madness Is Upon Us</p>
+      <h1>Let's take a look at our teams.</h1>
+    </div>
+  );
+}
+
+class TeamCard extends React.Component<Team> {
+  render() {
+    const oneTeam = this.props;
+
+    return (
+      <div>
+        <h2>School Name: {oneTeam.school}</h2>
+        <h2>Mascot: {oneTeam.name}</h2>
+        <h2>City: {oneTeam.city}</h2>
+        <h2>State: {oneTeam.state}</h2>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    // go out and get one band and return these things
+    <div>
+      {CollegeBasketballTeams.teams.map((team: Team) => (
+        <TeamCard key={team.school} {...team} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   // nothing is enforced by default so we have
   // to install ESLint to make consis
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <TeamList />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
+    {/* the result of the app.tsx is gonna happen ehre
+     */}
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
